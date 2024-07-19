0280d3bb4e784313ccbb6a8a10deff67bf2c1d1d
jhg51 <jhg51@humboldt.edu>
2024-07-19 08:35:04 -0700

    Added setup.py

---
+++ boto3manager/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='boto3manager',
+    version='0.0.1',
+    description='A Python package to manage Boto3 file uploads and downloads',
+    url='https://gitlab.nrp-nautilus.io/humboldt/boto3-manager.git',
+    author='John Gerving',
+    author_email='jhg51@humboldt.edu',
+    license='MIT',
+    packages=['boto3manager'],
+    zip_safe=False
+)
