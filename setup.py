f369289d629a2a6580036bd420fde19bd9b6403c
jhg51 <jhg51@humboldt.edu>
2024-07-19 08:37:37 -0700

    Changed version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='boto3manager',
-    version='0.0.1',
+    version='1.0',
     description='A Python package to manage Boto3 file uploads and downloads',
     url='https://gitlab.nrp-nautilus.io/humboldt/boto3-manager.git',
     author='John Gerving',
