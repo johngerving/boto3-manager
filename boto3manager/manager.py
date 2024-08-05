d8f7d095b7f1217ed4c63c273beae9b43ea02982
jhg51 <jhg51@humboldt.edu>
2024-08-05 10:09:31 -0700

    Added upload_files_recursive function

--- boto3manager/manager.py
+++ boto3manager/manager.py
@@ -5,36 +5,39 @@ from pathlib import Path
 import os
 
 class Boto3Manager:
-    def __init__(self, credentialsFilePath, profile, endpointUrl, bucketName):
+    def __init__(self, credentials_file_path, profile, endpoint_url, bucket_name):
         '''
-        :param credentialsFile: A path to a file containing S3 credentials
+        :param credentials_file_path: A path to a file containing S3 credentials
+        :param profile: The S3 profile to use from the credentials file
+        :param endpoint_url: The S3 endpoint to upload and download to and from
+        :param bucket_name: The name of the S3 bucket to use
         '''
 
-        assert credentialsFilePath is not None and isinstance(credentialsFilePath, str), "credentialsFilePath must be a string"
+        assert credentials_file_path is not None and isinstance(credentials_file_path, str), "credentials_file_path must be a string"
         assert profile is not None and isinstance(profile, str), "profile must be a string"
-        assert endpointUrl is not None and isinstance(endpointUrl, str), "endpointUrl must be a string"
-        assert bucketName is not None and isinstance(bucketName, str), "bucketName must be a string"
+        assert endpoint_url is not None and isinstance(endpoint_url, str), "endpoint_url must be a string"
+        assert bucket_name is not None and isinstance(bucket_name, str), "bucket_name must be a string"
 
         self.profile = profile
-        self.endpointUrl = endpointUrl
-        self.bucketName = bucketName
+        self.endpoint_url = endpoint_url
+        self.bucket_name = bucket_name
 
         # Read credentials from file
-        self.credentialsFile = configparser.ConfigParser()
-        self.credentialsFile.read(credentialsFilePath)
+        self.credentials_file = configparser.ConfigParser()
+        self.credentials_file.read(credentials_file_path)
 
-        self.accessKey = self.credentialsFile[profile]['aws_access_key_id']
-        self.secretKey = self.credentialsFile[profile]['aws_secret_access_key']
+        self.access_key = self.credentials_file[profile]['aws_access_key_id']
+        self.secret_key = self.credentials_file[profile]['aws_secret_access_key']
 
         # Initialize boto3 session
-        self.session = boto3.session.Session(aws_access_key_id=self.accessKey, aws_secret_access_key=self.secretKey)
+        self.session = boto3.session.Session(aws_access_key_id=self.access_key, aws_secret_access_key=self.secret_key)
         # Initialize boto3 client
         self.client = self.session.client(
             service_name='s3',
-            endpoint_url=self.endpointUrl
+            endpoint_url=self.endpoint_url
         )
 
-    def uploadAll(self, directory, destination=None):
+    def upload_all(self, directory, destination=None):
         '''Uploads all files from a directory to an S3 bucket
         
         :param directory: Directory to upload files from
@@ -47,29 +50,29 @@ class Boto3Manager:
         assert destination is None or isinstance(destination, str), "Destination must be a string"
 
         # Get the path of the directory to upload files from
-        directoryPath = Path(f'./{directory}')
+        directory_path = Path(f'./{directory}')
 
         # Check if directory exists
-        if directoryPath.is_dir():
+        if directory_path.is_dir():
             pass
         else:
             raise FileNotFoundError(f'Directory {directory} does not exist')
         
         # Get files inside directory recursively as posix paths
-        posixPaths = list(directoryPath.glob('**/*'))
+        paths = list(directory_path.glob('**/*'))
         # Convert to list of string paths
-        pathsToString = list(map(str, posixPaths))
+        paths_to_string = list(map(str, paths))
 
         # Loop through files in directory
-        for path in pathsToString:
+        for path in paths_to_string:
             try:
                 # Get name of file
-                objectName = os.path.basename(path)
+                object_name = os.path.base_name(path)
                 # Upload file
                 if destination is None:
-                    response = self.client.upload_file(path, self.bucketName, objectName)
+                    response = self.client.upload_file(path, self.bucket_name, object_name)
                 else:
-                    response = self.client.upload_file(path, self.bucketName, f"{destination}/{objectName}")
+                    response = self.client.upload_file(path, self.bucket_name, f"{destination}/{object_name}")
 
             except ClientError as e:
                 print(e)
@@ -77,13 +80,43 @@ class Boto3Manager:
         
         return True
     
-    def uploadFilesRecursive(self, path, destination):
-        '''Uploads files in a path recursively'''
+    def upload_files_recursive(self, path, destination=None):
+        '''Uploads files in a path recursively
+        
+        :param path: The path to upload files from
+        :param destination: The destination in the S3 bucket to upload to
 
+        :return: True if all uploads successful, False otherwise
+        '''
+
+        # Check parameters
+        assert path is not None and isinstance(path, str), "Path must be a string"
+        assert destination is None or isinstance(destination, str), "Destination must be a string"
+
+        # Convert path given into a Pure path
         path = Path(path)
-        print([x for x in path.glob('**/*')])
+        
+        # Ensure path exists
+        assert path.exists(), "Path does not exist"
+
+        # Get files in directory recursively, but only if they are not directories
+        files = [str(x) for x in path.glob('**/*') if not x.is_dir()]
+        
+        # Loop through files
+        for file in files:
+            try:
+                # Upload file - add destination as prefix if provided
+                if destination is None:
+                    self.client.upload_file(file, self.bucket_name, file)
+                else:
+                    self.client.upload_file(file, self.bucket_name, f"{destination}/{file}")
+            except ClientError as e:
+                print(e)
+                return False
+            
+        return True
     
-    def downloadFolder(self, folder, destination):
+    def download_folder(self, folder, destination):
         '''Downloads the contents of a folder from the S3 bucket
         
         :param folder: The folder to download from the S3 bucket
@@ -96,27 +129,27 @@ class Boto3Manager:
         assert destination is not None and isinstance(destination, str), "Destination must be a string"
 
         # Create destination directory if it doesn't exist
-        destinationPath = Path(destination)
-        destinationPath.mkdir(parents=True, exist_ok=True)
+        destination_path = Path(destination)
+        destination_path.mkdir(parents=True, exist_ok=True)
 
         # Get contents of bucket with prefix of folder
-        contents = self.client.list_objects_v2(Bucket=self.bucketName, Prefix=folder)['Contents']
+        contents = self.client.list_objects_v2(Bucket=self.bucket_name, Prefix=folder)['Contents']
         
         # Loop through the files in the folder
         for item in contents:
             try:
                 # Get base name of file
-                baseName = item['Key'].split('/')[-1]
+                base_name = item['Key'].split('/')[-1]
                 # Create a new file path in the destination directory
-                newFilePath = destinationPath / baseName
+                new_file_path = destination_path / base_name
                 # Download the file from the S3 bucket to the file path on the user's local machine
-                self.client.download_file(self.bucketName, item['Key'], newFilePath)
+                self.client.download_file(self.bucket_name, item['Key'], new_file_path)
             except ClientError as e:
                 print(e)
                 return False
         return True
     
-    def deleteFolder(self, folder):
+    def delete_folder(self, folder):
         '''Deletes a folder in the S3 bucket
         
         :param folder: Folder to delete in the S3 bucket
@@ -128,13 +161,13 @@ class Boto3Manager:
 
         try:
             # Get contents of the S3 bucket with the prefix of the folder
-            contents = self.client.list_objects_v2(Bucket=self.bucketName, Prefix=folder)['Contents']
+            contents = self.client.list_objects_v2(Bucket=self.bucket_name, Prefix=folder)['Contents']
 
             # Map contents of bucket to an array of dictionaries, each of the form {"Key": <filename>}
             objects = list(map(lambda x: {"Key": x["Key"]}, contents))
 
             # Delete all files in the folder from the bucket, using the mapped array of dictionaries
-            self.client.delete_objects(Bucket=self.bucketName, Delete={"Objects": objects})
+            self.client.delete_objects(Bucket=self.bucket_name, Delete={"Objects": objects})
         except ClientError as e:
             print(e)
             return False
