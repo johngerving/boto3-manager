af237e4ffc9462883a83a77edb5576918d2733c1
jhg51 <jhg51@humboldt.edu>
2024-09-19 08:43:33 -0700

    Added .gitignore and boto3manager.go

---
+++ boto3manager.go
@@ -0,0 +1,27 @@
+package boto3manager
+
+import (
+	"context"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+)
+
+type BucketBasics struct {
+	S3Client *s3.Client
+}
+
+func (basics BucketBasics) ListObjects(bucketName string) ([]types.Object, error) {
+	result, err := basics.S3Client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+		Bucket: aws.String(bucketName),
+	})
+	var contents []types.Object
+	if err != nil {
+		log.Printf("Couldn't list objects in bucket %v: %v\n", bucketName, err)
+	} else {
+		contents = result.Contents
+	}
+	return contents, err
+}
